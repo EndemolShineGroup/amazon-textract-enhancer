e88349e27476e2bd0b540dfdf3f22dda7a9cadb5
Joe Pietroni <joe.pietroni@endemolshine.com>
2020-02-25 11:49:08 +0000

    log table dict

--- functions/document-analysis-postprocess-table.py
+++ functions/document-analysis-postprocess-table.py
@@ -6,51 +6,51 @@ import time
 import boto3
 
 def lambda_handler(event, context):
-    
-    #Initialize Boto Resource	
+
+    #Initialize Boto Resource
     s3 = boto3.resource('s3')
     textract = boto3.client('textract')
     dynamodb = boto3.client('dynamodb')
-    table_name=os.environ['table_name']    
+    table_name=os.environ['table_name']
     file_list = []
 
-    if "Records" in event:        
+    if "Records" in event:
         records = event['Records']
         numRecords = len(records)
-        
+
         print("{} messages recieved".format(numRecords))
         for record in records:
             documentBlocks = None
             num_pages = 0
-            num_tables = 0    
+            num_tables = 0
             bucket = ""
-            upload_prefix = ""            
+            upload_prefix = ""
             textractJobId = ""
             textractStatus = ""
             textractAPI = ""
             textractJobTag = ""
             textractS3ObjectName = ""
-            textractS3Bucket = ""            
+            textractS3Bucket = ""
             if 'Sns' in record.keys():
-                sns = record['Sns']                               
+                sns = record['Sns']
                 if 'Message' in sns.keys():
                     message = json.loads(sns['Message'])
                     textractJobId = message['JobId']
                     print("{} = {}".format("JobId", textractJobId))
                     textractStatus = message['Status']
-                    print("{} = {}".format("Status",textractStatus))   
-                    textractTimestamp =  str(int(float(message['Timestamp'])/1000)) 
-                    print("{} = {}".format("Timestamp",textractTimestamp))     
+                    print("{} = {}".format("Status",textractStatus))
+                    textractTimestamp =  str(int(float(message['Timestamp'])/1000))
+                    print("{} = {}".format("Timestamp",textractTimestamp))
                     textractAPI = message['API']
-                    print("{} = {}".format("API", textractAPI))                    
+                    print("{} = {}".format("API", textractAPI))
                     textractJobTag = message['JobTag']
-                    print("{} = {}".format("JobTag", textractJobTag))    
+                    print("{} = {}".format("JobTag", textractJobTag))
                     documentLocation = message['DocumentLocation']
                     textractS3ObjectName = documentLocation['S3ObjectName']
-                    print("{} = {}".format("S3ObjectName", textractS3ObjectName))    
+                    print("{} = {}".format("S3ObjectName", textractS3ObjectName))
                     textractS3Bucket = documentLocation['S3Bucket']
-                    print("{} = {}".format("S3Bucket", textractS3Bucket))                          
-                    
+                    print("{} = {}".format("S3Bucket", textractS3Bucket))
+
                     bucket = textractS3Bucket
                     document_path = textractS3ObjectName[:textractS3ObjectName.rfind("/")] if textractS3ObjectName.find("/") >= 0 else ""
                     document_name = textractS3ObjectName[textractS3ObjectName.rfind("/")+1:textractS3ObjectName.rfind(".")] if textractS3ObjectName.find("/") >= 0 else textractS3ObjectName[:textractS3ObjectName.rfind(".")]
@@ -61,26 +61,28 @@ def lambda_handler(event, context):
                     else:
                         upload_prefix = "{}/{}".format(document_path, textractJobId)
 
-                    print("upload_prefix = " + upload_prefix)  
+                    print("upload_prefix = " + upload_prefix)
 
-                    num_pages, documentBlocks = GetDocumentAnalysisResult(textract, textractJobId) 
+                    num_pages, documentBlocks = GetDocumentAnalysisResult(textract, textractJobId)
 
             if documentBlocks is not None and len(documentBlocks) > 0:
                 print("{} Blocks retrieved".format(len(documentBlocks)))
 
                 #Extract table information  into a Python dictionary by parsing the raw JSON from Textract
                 tabledict = extractTableBlocks(documentBlocks)
-                
-                #Generate XML document using table information    
+
+                #Generate XML document using table information
                 num_tables, tables = generateTableXML(tabledict)
-                
+
+                print("{}".format(tabledict))
+
                 for page in tables:
                     for table in page:
                         html_document = "{}-page-{}-table-{}.html".format(document_name, table.attrib['ContainingPage'], table.attrib['TableNumber'])
                         html_file = open("/tmp/"+html_document,'w+')
                         html_file.write(prettify(table))
                         html_file.close()
-                        s3.meta.client.upload_file("/tmp/"+html_document, bucket, "{}/{}".format(upload_prefix,html_document))  
+                        s3.meta.client.upload_file("/tmp/"+html_document, bucket, "{}/{}".format(upload_prefix,html_document))
                         try:
                             response = dynamodb.update_item(
                                 TableName=table_name,
@@ -116,21 +118,21 @@ def lambda_handler(event, context):
                         }
                     )
                 except Exception as e:
-                    print('DynamoDB Insertion Error is: {0}'.format(e))       
-                    
+                    print('DynamoDB Insertion Error is: {0}'.format(e))
+
             s3_result = s3.meta.client.list_objects_v2(Bucket=bucket, Prefix="{}/".format(upload_prefix), Delimiter = "/")
             if 'Contents' in s3_result:
-                
+
                 for key in s3_result['Contents']:
                     if key['Key'].endswith("html"):
                         file_list.append("https://s3.amazonaws.com/{}/{}".format(bucket, key['Key']))
-                
+
                 while s3_result['IsTruncated']:
                     continuation_key = s3_result['NextContinuationToken']
                     s3_result = s3.meta.client.list_objects_v2(Bucket=bucket, Prefix="{}/".format(upload_prefix), Delimiter="/", ContinuationToken=continuation_key)
                     for key in s3_result['Contents']:
                         if key['Key'].endswith("html"):
-                            file_list.append("https://s3.amazonaws.com/{}/{}".format(bucket, key['Key']))            
-            print(file_list)   
-        
+                            file_list.append("https://s3.amazonaws.com/{}/{}".format(bucket, key['Key']))
+            print(file_list)
+
     return file_list
